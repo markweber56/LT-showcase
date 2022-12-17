246d7bb25c49532c1729a5bcc6d7b19c34833593
mark weber <markweber56@gmail.com>
2022-12-16 19:47:58 -0600

    refactor, add conjured item logic and tests

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -3,28 +3,6 @@ import unittest
 
 from gilded_rose import Item, GildedRose
 
-'''
-	- All items have a SellIn value which denotes the number of days we have to sell the item
-	- All items have a Quality value which denotes how valuable the item is
-	- At the end of each day our system lowers both values for every item
-
-Pretty simple, right? Well this is where it gets interesting:
-
-	-- Once the sell by date has passed, Quality degrades twice as fast
-	-- The Quality of an item is never negative
-	-- "Aged Brie" actually increases in Quality the older it gets
-	-- The Quality of an item is never more than 50
-	- "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
-	- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
-	Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
-	Quality drops to 0 after the concert
-
-We have recently signed a supplier of conjured items. This requires an update to our system:
-
-	- "Conjured" items degrade in Quality twice as fast as normal items
-'''
-
-
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
         print("test foo")
@@ -100,6 +78,10 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEqual(11, items[0].quality)
 
     def test_update_backstage_pass_10_days(self):
+        '''
+        NOTE: the old logic is bad for this. "Backstage pass" quality is updated
+        based on previous sell_in value rather than updated one
+        '''
         print("quality of concert 10 days away should increase by 2")
         items = [
             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=10)
@@ -135,6 +117,32 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEqual(2, items[0].sell_in)
         self.assertEqual(50, items[0].quality)
 
+    def test_conjured_quality_decreases_by_two(self):
+        print("quality of conjured item should decrease by 2 if not expired")
+        items = [
+            Item(name="Conjured Mana Cake", sell_in=5, quality=10)
+        ]
+        GildedRose(items).update_quality()
+        self.assertEqual(4, items[0].sell_in)
+        self.assertEqual(8, items[0].quality)
+
+    def test_conjured_quality_decreases_by_four_once_expired(self):
+        print("quality of conjured item should decrease by 4 once expired")
+        items = [
+            Item(name="Conjured Mana Cake", sell_in=-3, quality=10)
+        ]
+        GildedRose(items).update_quality()
+        self.assertEqual(-4, items[0].sell_in)
+        self.assertEqual(6, items[0].quality)
+
+    def test_conjured_item_quality_does_not_become_negative(self):
+        print("quality of conjured item should not become negative")
+        items = [
+            Item(name="Conjured Mana Cake", sell_in=-3, quality=2)
+        ]
+        GildedRose(items).update_quality()
+        self.assertEqual(-4, items[0].sell_in)
+        self.assertEqual(0, items[0].quality)
 
 if __name__ == '__main__':
     unittest.main()
